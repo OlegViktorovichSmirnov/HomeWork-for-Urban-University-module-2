a3140b1638a43abf563935e996af8e356a567744
Олег <solegva@ya.ru>
2024-11-03 17:51:11 +0300

    03/11 +m_2_5

--- module_2_5.py
+++ module_2_5.py
@@ -30,5 +30,18 @@
 # В случае передачи аргумента со значением 0 или меньше, будет возвращаться пустой список.
 
 def get_matrix(n, m, value):
-    matrix = 0
-    for n in matrix():
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+print('-------')
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print('-------')
+print(result2)
+print('-------')
+print(result3)
